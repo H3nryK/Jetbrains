40b8cd825f105cd0125a29cc19e8c8a4e718e98f
H3nryK <henrykimani89@gmail.com>
2024-06-15 23:08:27 +0300

    backend

--- backend/jet/models.py
+++ backend/jet/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Enrollment(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.IntegerField()
+    course = models.CharField()
+    info = models.TextField()
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    phone = models.IntegerField()
+    message = models.TextField()
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+class Testimony(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+    course = models.CharField(max_length=255)
+    message = models.TextField()
+    time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
