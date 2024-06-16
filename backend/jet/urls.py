aea69d229246d92c3ad8836933292cf29804f7bd
H3nryK <henrykimani89@gmail.com>
2024-06-16 15:08:57 +0300

    enroll

--- backend/jet/urls.py
+++ backend/jet/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', main_view, name="main"),
+    path('courses/', course_view, name='courses'),
+    path('enroll/', enroll_view, name='enroll'),
 ]
--- backend/jet/views.py
+++ backend/jet/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 def main_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('number')
+        message = request.POST.get('message')
+
+        contact, created = Contact.objects.get_or_create(
+            name = name,
+            email = email,
+            phone = phone,
+            message = message
+        )
+
+        messages.success(request, "Thank you for contacting us.")
+
+        return redirect('main')
+
     return render(request, 'main.html')
+
+def course_view(request):
+    return render(request, 'courses.html')
+
+def enroll_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        course = request.POST.get('course')
+        info = request.POST.get('message')
+
+        enroll, created = Enrollment.objects.get_or_create(
+            name = name,
+            email = email,
+            phone = phone,
+            course = course,
+            info = info
+        )
+
+        messages.success(request, "Thank you for Joining us.")
+
+        return redirect('enroll')
+    
+    return render(request, 'enroll.html')
