d65b1ff78da304574de962bef11f252836a37184
H3nryK <henrykimani89@gmail.com>
2024-08-11 20:15:17 +0300

    sitemap & robot.txt

--- jet/urls.py
+++ jet/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 from django.contrib.auth.views import LogoutView
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', main_view, name="main"),
@@ -11,6 +12,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='main'), name='logout'),
     path('testify/', testify_view, name='testify'),
 
+    path('sitemap/', TemplateView.as_view(template_name="sitemap.xml"), name="sitemap"),
+
     path('dashboard/', dashboard_view, name='dashboard'),
     path('approve_testimonial/<int:testimonial_id>/', approve_testimonial, name='approve_testimonial'),
+
 ]
